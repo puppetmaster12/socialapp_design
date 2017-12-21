b99ce8a1e6b8644b73f5ed07d1226abaf2e598e1
puppetmaster12 <pavithranthilakanathan@gmail.com>
2017-12-21 21:07:43 +0530

    First commit

---
+++ home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/home.css">
+    <script type="text/javascript" src="js/home.js"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
